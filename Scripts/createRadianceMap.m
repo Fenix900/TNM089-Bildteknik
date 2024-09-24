015d4e840c8cf2a0427dfb681051fdf718b47095
Adam Nilsson <nilssonadam9801@gmail.com>
2024-09-24 17:52:20 +0200

    createRadianceMap

--- Scripts/createRadianceMap.m
+++ Scripts/createRadianceMap.m
@@ -4,12 +4,17 @@ radianceMap = zeros(row, col);
 
 for i = 1:col
     for k = 1:row
+        num = 0;
+        den = 0;
         for j = 1:imageAmount
             pixel = images(k,i,j) + 1;
-            
-            randianceMap(k,i) = (w(pixel) * (g(pixel) - B(j))) / w(pixel);
+
+            num =  num + radianceMap(k,i) + (w(pixel) * (g(pixel) - B(j)));
+            den = den + w(pixel);
+            %radianceMap(k,i) = radianceMap(k,i) + ((w(pixel) * (g(pixel) - B(j))) / w(pixel));
             
         end
+        radianceMap(k,i) = num / den;
     end
 end
 
